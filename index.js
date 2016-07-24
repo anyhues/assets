64d73183186c70157200eb20298cce45d0e01987
Falk Hoppe <falkhoppe81@gmail.com>
2016-07-24 22:54:04 +0200

    fixed revving

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const path = require('path')
 
 module.exports = {
-  templatePath: path.resolve(__dirname, 'templates')
+  templatePath: path.resolve(__dirname, 'templates'),
+  revManifest: path.resolve(__dirname, 'rev-manifest.json')
 }
