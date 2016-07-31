20f1626fb135898e629e5e627cc4582a0741f614
Falk Hoppe <falkhoppe81@gmail.com>
2016-07-31 19:36:37 +0200

    added shared config

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ const path = require('path')
 
 module.exports = {
   templatePath: path.resolve(__dirname, 'templates'),
-  revManifest: path.resolve(__dirname, 'rev-manifest.json')
+  revManifest: path.resolve(__dirname, 'rev-manifest.json'),
+  configs: {
+    development: {
+      'assetHost': 'http://localhost:8235',
+      'homeHost': 'http://localhost:8234',
+      'couplesHost': 'http://localhost:8236',
+      'familyHost': 'http://localhost:8237'
+    },
+    production: {
+      'assetHost': 'https://niehues-assets.github.io',
+      'homeHost': 'https://www.praxis-niehues.de',
+      'couplesHost': 'http://www.paartherapeut-essen.de',
+      'familyHost': 'http://www.familientherapeut-essen.de'
+    }
+  }
 }
