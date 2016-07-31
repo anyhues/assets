fa35355e4ff4520284e1a6691b407e8cc7a1bbdb
Falk Hoppe <falkhoppe81@gmail.com>
2016-07-31 20:21:46 +0200

    added image tmp folder

--- index.js
+++ index.js
@@ -8,13 +8,24 @@ module.exports = {
       'assetHost': 'http://localhost:8235',
       'homeHost': 'http://localhost:8234',
       'couplesHost': 'http://localhost:8236',
-      'familyHost': 'http://localhost:8237'
+      'familyHost': 'http://localhost:8237',
+      'singlesHost': 'http://localhost:8238'
     },
     production: {
       'assetHost': 'https://niehues-assets.github.io',
-      'homeHost': 'https://www.praxis-niehues.de',
+      'homeHost': 'http://www.praxis-niehues.de',
       'couplesHost': 'http://www.paartherapeut-essen.de',
-      'familyHost': 'http://www.familientherapeut-essen.de'
+      'familyHost': 'http://www.familientherapeut-essen.de',
+      'singlesHost': 'http://www.sexualtherapeut-essen.de'
+    }
+  },
+  templateGlobals: (config) => {
+    return {
+      ASSET_HOST: config.get('assetHost'),
+      COUPLES_HOST: config.get('couplesHost'),
+      FAMILY_HOST: config.get('couplesHost'),
+      HOME_HOST: config.get('homeHost'),
+      SINGLES_HOST: config.get('singlesHost')
     }
   }
 }
